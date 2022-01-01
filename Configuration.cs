12eaaac9c04f7e67245b2c42ebd398584cea5640
dhtgaming <47740790+dhtgaming@users.noreply.github.com>
2022-01-01 22:09:11 +0100

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KNN
+{
+    public class Configuration
+    {
+        public string Separator { get; set; }
+        public int LiczbaLinii { get; set; }
+        public int LiczbaKolumn { get; set; }
+        public List<List<string>> ZakresAtrybutow { get; set; }
+        public List<double> WartosciMinimalneWKolumnie { get; set; }
+        public List<double> WartosciMaksymalneWKolumnie { get; set; }
+        public List<string> TypyDanych { get; set; }
+        public string ZnakZapytania { get; set; }
+        public int Decyzja { get; set; }
+    }
+}
